b131e2810867b8e4a3ff7377ab75d5ad09fff5cf
Franco Sarachu <fsarachu@gmail.com>
2016-09-04 20:22:00 -0300

    Add conosole log with operations info

--- rename_files.py
+++ rename_files.py
@@ -9,11 +9,23 @@ def rename_files():
 
     # 2. Get file names from images/ folder
     file_list = os.listdir(".")
-    print file_list
+    # print file_list
 
     # 3. For each file name, delete the numbers from it
+    rename_count = 0
+
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(None, "0123456789"))
+        old_name = file_name
+        new_name = file_name.translate(None, "0123456789")
+
+        print "Renaming file \"" + old_name + "\" to \"" + new_name + "\""
+
+        os.rename(old_name, new_name)
+
+        rename_count += 1
+
+    print ""
+    print "Finished: " + rename_count.__str__() + " files renamed"
 
 
 rename_files()
