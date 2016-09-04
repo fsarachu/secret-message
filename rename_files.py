f201658d5b7a648e9769dbb5bb40c558dc4c5b9e
Franco Sarachu <fsarachu@gmail.com>
2016-09-04 19:40:02 -0300

    Get file names from images/ folder

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,12 @@
+import os
+
+
 def rename_files():
     # 1. Get file names from images/ folder
+    file_names = os.listdir(r"images/")
+    print file_names
+
     # 2. For each file name, delete the numbers from the string
-    print "Dummy statement"
 
 
 rename_files()
