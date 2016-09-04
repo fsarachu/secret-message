80bf7f326be5c20adad6e441b53e10b6fe37c52f
Franco Sarachu <fsarachu@gmail.com>
2016-09-04 20:15:02 -0300

    Finish it up

--- rename_files.py
+++ rename_files.py
@@ -2,11 +2,18 @@ import os
 
 
 def rename_files():
-    # 1. Get file names from images/ folder
-    file_names = os.listdir(r"images/")
-    print file_names
+    # 1. Find the images directory and get into it
+    cwd = os.getcwd()
+    files_dir = cwd + "/images"
+    os.chdir(files_dir)
 
-    # 2. For each file name, delete the numbers from the string
+    # 2. Get file names from images/ folder
+    file_list = os.listdir(".")
+    print file_list
+
+    # 3. For each file name, delete the numbers from it
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
 
 
 rename_files()
